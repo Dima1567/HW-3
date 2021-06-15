d6d9a7c505c93eef0503af3f62ff7dd686f24a08
Dima1567 <7833288d@gmail.com>
2021-06-15 19:55:03 +0300

    Initial Commit

---
+++ HW-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-3
+//
+//  Created by Dima Savitskiy on 15.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
