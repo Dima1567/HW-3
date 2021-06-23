fab679b73f5662ece3484cffcd3809e41082aee4
Dima1567 <7833288d@gmail.com>
2021-06-23 10:35:29 +0300

    second screen

--- HW-3/ViewController.swift
+++ HW-3/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var maxValueLabel: UILabel!
+    @IBOutlet weak var minValueLabel: UILabel!
+    @IBOutlet weak var randomValueLabel: UILabel!
+    @IBOutlet weak var getRandomNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getRandomNumberButton.layer.cornerRadius = 10
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else {return}
+        settingsVC.maxValue = self.maxValueLabel.text ?? "100"
+        settingsVC.minValue = self.minValueLabel.text ?? "0"
+    }
+
+    
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        guard let settingsVC = unwindSegue.source as? SettingsViewController else {return}
+        minValueLabel.text = settingsVC.minimumTextField.text
+        maxValueLabel.text = settingsVC.maximumTextField.text
+    }
+    
+    
+    @IBAction func getRandomNumberButton(_ sender: UIButton) {
+        let minNumber = Int(String(minValueLabel.text ?? "")) ?? 0
+        let maxNumber = Int(String(maxValueLabel.text ?? "")) ?? 100
+        
+        randomValueLabel.text = String(Int.random(in: minNumber...maxNumber))
+        }
     }
 
 
-}
+
 
